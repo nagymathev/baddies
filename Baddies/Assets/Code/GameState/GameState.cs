ea0c4b680ca06523d5d9f5b65a4c1c64db032bf6
Peter Nagymathe <peter.nagymathe@gmail.com>
2023-07-08 18:08:36 +0100

    game state to monobehaviour

--- Baddies/Assets/Code/GameState/GameState.cs
+++ Baddies/Assets/Code/GameState/GameState.cs
@@ -1,4 +1,7 @@
-﻿namespace Code.GameState
+﻿using System;
+using UnityEngine;
+
+namespace Code.GameState
 {
     public class GameState
     {
@@ -9,6 +12,16 @@
             money = 1000;
             // Register that when we spawn minion we reduce our money
             EventManager.Instance.OnMinionSpawned += (_, x) => money -= 1;
+        }
+    }
+
+    public class StateBehaviour : MonoBehaviour
+    {
+        public GameState State { get; private set; }
+
+        private void Start()
+        {
+            State = new GameState();
         }
     }
 }
